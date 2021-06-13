72fcad09fdf4ef3fb9e77eef8f4318768a4a0aca
xaviergascon <xgascon04@gmail.com>
2021-06-13 13:41:06 +0200

    delete merging messages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 
 module.exports = {
@@ -11,18 +10,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle-[name].js',
   },
-=======
-const path = require('path');
-
-module.exports = {
-  entry: {
-    index: './src/index.js',
-    photographer: './src/photographer.js', 
-    tags: './src/tags.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle-[name].js',
-  },
->>>>>>> 6e2008b7b6b65abde9df2622d42fb2cf272ef3af
 };
